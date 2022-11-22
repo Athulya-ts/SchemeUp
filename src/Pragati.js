10b1e373dcc216244e7734632660326e6ddd341e
Athulya-ts <113665715+Athulya-ts@users.noreply.github.com>
2022-11-22 22:19:02 +0530

    Update Pragati.js

--- src/Pragati.js
+++ src/Pragati.js
@@ -1,14 +1,17 @@
 import "./Pragati.css";
+import { Link } from "react-router-dom";
 import image9Arrow from "./assets/image9Arrow.png";
 const Pragati = () => {
   return (
     <div className="pragati">
-      <img className="image-9" src={image9Arrow} alt="9"/>
-      <span className="pragati-scholarship">
+      <Link to="/Natschemes">
+      <img className="image-9Arrow" src={image9Arrow} alt="9"/>
+      </Link>
+      <h3>
         PRAGATI SCHOLARSHIP SCHEME FOR GIRLS STUDENT
-      </span>
+      </h3><br></br>
         <div className="rectangle-33">
-          <span className="pragati-is-a-mhrd-sc">
+          <p className="pragati-is-a-mhrd-sc">
             <p>Pragati is a MHRD Scheme being implemented by of AICTE aimed at
             providing assistance for Advancement of Gsirls pursuing Technical
             Education. </p>
@@ -20,24 +23,24 @@ const Pragati = () => {
             Books&#x2F;Equipment&#x2F;Softwares&#x2F;
             Laptop&#x2F;Desktop&#x2F;Vehicle&#x2F;Fee paid towards competitive
             examination applications forms&#x2F;exam.</p>
-          </span>
+          </p>
         </div>
       <div className="rectangle-32">
-        <span className="reservation-15-for">
+        <p className="reservation-15-for">
           <p><b>Reservation-</b> 15% for SC, 7.5% for ST and 27% for OBC
-          candidate&#x2F;applicant. </p><p><b>Document Required :</b>1. Mark Sheet of standard
+          candidate&#x2F;applicant. </p><p><b>Document Required : </b>1. Mark Sheet of standard
           Xth &#x2F;XIIth &#x2F; others as applicable.</p><p> 2. Annual family Income
           Certificate for the preceding financial year in the prescribed format
           issued by not below the rank of Tahsildar. </p><p>3. Admission letter issued
           by Directorate of Technical Education for the admission in
           Diploma&#x2F;Degree course.</p> <p>4. Certificate issued by the
           Director&#x2F;Principal&#x2F; Head of the Institute.</p>
-        </span>
+        </p>
       </div>
       <br></br>
-      <button className="rectangle-28">
-        <span className="apply">Apply</span>
-      </button>
+      <div className="rectangle-28">
+        <p className="apply"><a href="https://scholarships.gov.in/">Apply</a></p>
+      </div>
     </div>
   );
 };
